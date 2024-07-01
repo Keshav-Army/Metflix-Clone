a3f154843d18a53853147622b6e6783cc8fab385
Keshav Sharma <keshavsharmao210@gmail.com>
2024-07-01 18:56:16 +0530

    asd

---
+++ script.js
@@ -0,0 +1,17 @@
+// console.log(typeof ("3" + 3))
+// console.log(typeof ("6" - 3))
+
+// console.log("2" + 3 * 4)
+
+// var a = 5
+// console.log(a++)    //console.log(++a)
+// console.log(a)     //console.log(a)
+
+// console.log(NaN === NaN)
+
+// console.log(true < 3)  // ture > 3 = ture
+// console.log(3 > 2 > 1)  // ture > 1 = false
+
+// var arr = [0, 1, 2, 3, 4, 5];
+// var res = arr.filter((a) => a < 3).map((b) => b * 2).reduce((c, d) => c + d)
+// console.log(res)
